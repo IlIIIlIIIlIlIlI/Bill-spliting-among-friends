0dac2b16bb2d01c07f02aec1eac11d59f7ffe6b7
Sanket Jadhav <dev.hexanovate1@gmail.com>
2022-11-20 17:05:22 +0530

    start of big application splitwise

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ const baseClass = 'main-app';
 function App() {
   return (
     <div className={`${baseClass}__header`}>
-      <div>{getDividedValueUpto2ndDecimal(0)}</div>
+      <div>
+        <pre>{getDividedValueUpto2ndDecimal(3.0)}</pre>
+      </div>
     </div>
   );
 }
--- src/Utility/NumberUtil.ts
+++ src/Utility/NumberUtil.ts
@@ -1,10 +1,10 @@
 export const getDividedValueUpto2ndDecimal = (value: number) => {
   if (value === 0) return 0.0;
-  const quotient = String(Math.trunc(value * 100));
-  return Number(
-    `${quotient.substring(0, quotient.length - 3)}.${quotient.substring(
-      quotient.length - 2,
-      quotient.length
-    )}`
+  const quotient = Math.trunc(value * 100).toString();
+  const digitsBeforeDecimalPoint = quotient.substring(0, quotient.length - 2);
+  const digitsAfterDecimalPoint = quotient.substring(
+    quotient.length - 2,
+    quotient.length
   );
+  return Number(`${digitsBeforeDecimalPoint}.${digitsAfterDecimalPoint}`);
 };
