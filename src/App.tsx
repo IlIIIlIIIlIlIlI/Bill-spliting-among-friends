6b2e2c707fa81e5715f2696a5610a3378b88327b
Suraj Jaiswal <96817675+IlIIIlIIIlIlIlI@users.noreply.github.com>
2022-11-20 17:29:51 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className={`${baseClass}__header`}>
       <div>
-        <pre>{getDividedValueUpto2ndDecimal(2.0189)}</pre>
+        <pre>{getDividedValueUpto2ndDecimal(2.0189438989)}</pre>
       </div>
     </div>
   );
